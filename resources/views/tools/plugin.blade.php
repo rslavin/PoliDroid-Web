c3d7f769df6b0e72dadef52ee6542379f02afc72
Rocky Slavin <rocky.slavin@gmail.com>
2016-11-16 20:25:23 -0600

    fixed iframe

--- resources/views/tools/plugin.blade.php
+++ resources/views/tools/plugin.blade.php
@@ -9,6 +9,13 @@
         <link href="{{ asset('/polidroidvideo/polidroid-tutorial_embed.css') }}" rel="stylesheet">
 
         <iframe class="tscplayer_inline" id="embeddedSmartPlayerInstance" src="/polidroidvideo/polidroid-tutorial_player.html?embedIFrameId=embeddedSmartPlayerInstance" scrolling="no" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
+
+        <h2>Download</h2>
+        <ul>
+            <li><a href="/downloads/PoliDroid-AS.zip">Plugin</a></li>
+            <li><a href="/downloads/mappings.csv">Mappings File</a></li>
+            <li><a href="/downloads/ontology.owl">Ontology File</a></li>
+        </ul>
     </div>
 
 @stop()
