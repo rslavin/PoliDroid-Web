9af931c7f5aa55e62b13fed0f80145275f41385b
Rocky Slavin <rocky.slavin@gmail.com>
2016-11-16 20:51:20 -0600

    updated text

--- resources/views/tools/plugin.blade.php
+++ resources/views/tools/plugin.blade.php
@@ -4,13 +4,13 @@
         <h2>Polidroid-AS</h2>
 
         <p>Polidroid-AS is an Android Studio plugin which helps developers to create and align privacy policies for
-            their source code.</p>
+            their source code. The video below describes its usage and features.</p>
         <hr>
         <link href="{{ asset('/polidroidvideo/polidroid-tutorial_embed.css') }}" rel="stylesheet">
 
         <iframe class="tscplayer_inline" id="embeddedSmartPlayerInstance" src="/polidroidvideo/polidroid-tutorial_player.html?embedIFrameId=embeddedSmartPlayerInstance" scrolling="no" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
 
-        <h2>Download</h2>
+        <h3>Download</h3>
         <ul>
             <li><a href="/downloads/PoliDroid-AS.zip">Plugin</a></li>
             <li><a href="/downloads/mappings.csv">Mappings File</a></li>
