d9a8780dbd90105b6ed6de37245b55b70bb31040
Rocky Slavin <rocky.slavin@gmail.com>
2016-11-16 21:04:08 -0600

    updated publications

--- resources/views/site/publications.blade.php
+++ resources/views/site/publications.blade.php
@@ -10,18 +10,27 @@
 
         <div class="box-body">
             <ul>
+                <li>Mitra Bokaei Hosseini, Sudarshan Wadkar, Travis D. Breaux, Jianwei Niu. Lexical Similarity of
+                    Information Type Hypernyms, Meronyms and Synonyms in Privacy Policies. <i>AAAI Fall Symposium on
+                    Privacy and Language Technologies (PLT)</i>, Arlington, Virginia, Nov. 2016. (<a href="https://www.cs.cmu.edu/~breaux/publications/hosseini-aaai16.pdf">pdf</a>)
+                </li>
+                <li>
+                    Rocky Slavin. "Does your Android App Collect More than it Promises to?", <i>IEEE Software Blog</i>,
+                    2016, <a href="http://blog.ieeesoftware.org/2016/05/does-your-android-app-collect-more-than.html">
+                        http://blog.ieeesoftware.org/2016/05/does-your-android-app-collect-more-than.html</a>.
+                </li>
                 <li>
                     Rocky Slavin, Xiaoyin Wang, Mitra Hosseini, William Hester, Ram Krishnan, Jaspreet
                     Bhatia, Travis D. Breaux, and Jianwei Niu. "Toward a Framework for Detecting Privacy Policy
                     Violation in Android Application Code", <i> 38th ACM/IEEE International Conference on Software
-                        Engineering</i>, 2016, Austin, Texas. (<a
+                        Engineering</i>,  Austin, Texas, 2016. (<a
                             href="http://galadriel.cs.utsa.edu/~rslavin/publications/icse16.pdf">pdf</a>)
                 </li>
                 <li>
                     Rocky Slavin, Xiaoyin Wang, Mitra Hosseini, William Hester, Ram Krishnan, Jaspreet
                     Bhatia, Travis D. Breaux, and Jianwei Niu. "PVDetector: A Detector of Privacy Policy Violations for
                     Android Apps", <i>Proceedings of International Conference on Mobile Software Engineering and Systems
-                        (MOBILESoft)</i>, 2016, Austin, Texas. (<a
+                        (MOBILESoft)</i>, Austin, Texas, 2016. (<a
                             href="http://galadriel.cs.utsa.edu/~rslavin/publications/mobilesoft16.pdf">pdf</a>)
                 </li>
             </ul>
